5f07ab0e2944743e6b7ebb89ba1960a50bc8ce27
nicolaujoan <jnicolausantandreu@cifpbmoll.eu>
2021-12-27 13:32:40 +0100

    :hammer: add constant class variables

---
+++ scoreCard.py
@@ -0,0 +1,16 @@
+from _typeshed import Self
+
+
+class scoreCard():
+
+    ### GLOBALS ###
+    STRIKE = 'X'
+    SPARE = '/'
+    NO_PINS_BOWLED = '-'
+    BOWLING_SYMBOLS = ['X', '/', '-']
+    LAST_FRAME_ROLL = 19
+
+   
+    
+
+        
